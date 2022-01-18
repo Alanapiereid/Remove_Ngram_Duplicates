40ad605439bd58997620f58222e3ba731c10cb45
Alan Reid <56002246+Alanapiereid@users.noreply.github.com>
2022-01-18 10:54:07 +0100

    Update Remove_Ngram_Duplicates.py

--- Remove_Ngram_Duplicates.py
+++ Remove_Ngram_Duplicates.py
@@ -1,48 +1,53 @@
 from nltk import ngrams
 from collections import Counter
 
-def get_grams(text, n):
-    text = text.lower()
-    trigrams = ngrams(text.split(), n)
-    collection = []
+class duplicate_rem:
+    def __init__(self, text):
+        self.text = text
 
-    for x in trigrams:
-        collection.append(' '.join(x))
+    def get_grams(self, text, n):
 
-    counter = Counter
+        text = text.lower()
+        n_grams = ngrams(text.split(), n)
+        collection = []
 
-    obj = counter(collection)
+        for x in n_grams:
+            collection.append(' '.join(x))
 
-    drop_words = []
-    for key in obj:
-        if obj[key] > 1:
-            drop_words.append(key)
-            
-    text = text.replace(drop_words[0], "")
-    text = text.replace("  ", ' ' + drop_words[0] + ' ')
-    return text.strip().capitalize()
+        counter = Counter
 
-def attempt(text):
-    the_list = []
-    try:
-        the_list.append(get_grams(text, 4))
-    except IndexError:
+        obj = counter(collection)
+
+        drop_words = []
+        for key in obj:
+            if obj[key] > 1:
+                drop_words.append(key)
+                
+        text = text.replace(drop_words[0], "")
+        text = text.replace("  ", ' ' + drop_words[0] + ' ')
+        return text.strip().capitalize()
+
+    def attempt(self):
+        the_list = []
+        try:
+            the_list.append(self.get_grams(self.text, 4))
+        except IndexError:
+                pass
+        try:
+            the_list.append(self.get_grams(self.text, 3))
+        except IndexError:
             pass
-    try:
-        the_list.append(get_grams(text, 3))
-    except IndexError:
-        pass
-    try:
-        the_list.append(get_grams(text, 2))
-    except IndexError:
-        pass
-    return the_list
+        try:
+            the_list.append(self.get_grams(self.text, 2))
+        except IndexError:
+            pass
+        return the_list[0]
 
-sentence = "The thing the thing is I hate you so much, it's amazing"
-sentence2 = "The thing is really the thing is really I hate you so much, it's amazing"
+sentence = "The thing the thing is I love you so much, it's amazing"
+sentence2 = "The thing is really the thing is really I love you so much, it's amazing"
 
-print(attempt(sentence)[0])
-# The thing is i hate you so much, it's amazing
-print(attempt(sentence2)[0])
-# The thing is really i hate you so much, it's amazing
+print(duplicate_rem(sentence).attempt())
+# The thing is i love you so much, it's amazing
+print(duplicate_rem(sentence2).attempt())
+# The thing is really i love you so much, it's amazing
 
